10399fca73b971ca487d2d72ccbc907a874a1e42
Mmathuso Angelina <48378806+Meythusoo98@users.noreply.github.com>
2022-11-07 22:53:01 +0200

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+ 
+function About(){
+    return <div><h1>About Me</h1><p>About my page to be done</p>
+     <p><Link href="/">Home</Link></p></div>
+   
+ }
+
+ export default About
---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+import Link from "next/link"
+
+function Contact(){
+    return <div><h1>Contact Me</h1><p>Check me out now on the following</p>
+    <p><Link href="/">Home</Link></p></div>
+}
+export default Contact
